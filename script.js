1a884cdbc8496866e615e68f610ed44e8a80bd9d
MahroshZahid <157431935+MahroshZahid@users.noreply.github.com>
2024-04-04 02:21:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//navbar fixed top scroll white
 document.addEventListener('DOMContentLoaded', function() {
     const header = document.querySelector('.navbar');
 
@@ -10,5 +12,36 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 });
+
+//submitting contact form
+function submitForm() {
+    //retrieving data
+    var namef = document.getElementById("firstName").value;
+    var namel = document.getElementById("lastName").value;
+    var emailaddress = document.getElementById("emailadd").value;
+    var phone_num = document.getElementById("phone").value;
+    var message = document.getElementById("message").value;
+
+
+    //storing data in local storage
+    var first_name = localStorage.setItem("namef", namef);
+    var last_name = localStorage.setItem("namel", namel);
+    var email_add = localStorage.setItem("emailaddress", emailaddress);
+    var phone_number = localStorage.setItem("phone_num", phone_num);
+    var msg = localStorage.setItem("message", message);
+
+    //confirmation message
+    alert("Thank you for your submission! Please expect a response within 2-3 days :)")
+}
+
+function changeFontSize(fontSize) {
+    // all text elements selected on the page
+    var textElements = document.querySelectorAll('.hero_title, .accordion-body, p, .hero_title, .hero_desc, .nav-link, .navbar-brand, h3, .form-label, .about-paragraph, .contact-header, .contact-subheader, .contact-text, .btn, .experience-header');
+    
+    // Loop through each text element and set its font size
+    textElements.forEach(function(element) {
+        element.style.fontSize = fontSize;
+    });
+}
 
 
